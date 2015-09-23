27728784713420f828f57151f0d1e58fbe7048df
LeeYouth <yuanliyong520@126.com>
2015-09-23 17:07:58 +0800

    Initial Commit

---
+++ CrashManagerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CrashManagerDemo
+//
+//  Created by LYoung on 15/9/23.
+//  Copyright (c) 2015年 LYoung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CrashManagerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CrashManagerDemo
+//
+//  Created by LYoung on 15/9/23.
+//  Copyright (c) 2015年 LYoung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
